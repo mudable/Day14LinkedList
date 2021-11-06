a275ae9b913a24e70462adbb58bcc789d9640e81
mudable <mudableshilpa@gmail.com>
2021-11-06 17:32:08 +0530

    Added : UC4 to perform operation

--- src/com/bridgelabz/linkedlist/Stack.java
+++ src/com/bridgelabz/linkedlist/Stack.java
@@ -3,33 +3,67 @@ package com.bridgelabz.linkedlist;
 import com.bridgelabz.linkedlist.*;
 
 public class Stack {
-	private MyLinkedList myLinkedList;
+	    /*
+	     * To create stack operations
+	     * */
 
-	/*
-	 * Constructor
-	 */
-	public Stack() {
-		this.myLinkedList = new MyLinkedList();
-	}
+	    private MyLinkedList myLinkedList;
 
-	/*
-	 * used to push nodes
-	 */
-	public void push(INode myNode) {
-		myLinkedList.add(myNode);
+	    /*
+	     *  Constructor
+	     */
+	    public Stack() {
+	        this.myLinkedList = new MyLinkedList();
+	    }
 
-	}
+	    /*
+	     * used to push nodes
+	     */
+	    public void push(INode myNode) {
+	        myLinkedList.add(myNode);
 
-	public INode peak() {
-		return myLinkedList.head;
-	}
+	    }
 
-	public void printStack() {
-		myLinkedList.printNodes();
-	}
+	    /*
+	     * used to check the head
+	     * */
+	    public INode peak() {
+	        return myLinkedList.head;
+	    }
 
-	public void enqueu(INode myNode) {
-		myLinkedList.append(myNode);
-	}
 
+	    /*
+	     * pop is used to delete nodes
+	     * */
+	    public INode pop() {
+	        return myLinkedList.pop();
+	    }
+
+	    /*
+	     * Used to check if queue is empty or not
+	     * */
+	    public boolean isEmpty() {
+	        return myLinkedList.head == null;
+	    }
+
+	    /*
+	     * print stack
+	     * */
+	    public void printStack() {
+	        myLinkedList.printNodes();
+	    }
+
+	    /*
+	     * used to add elements
+	     * */
+	    public void enqueu(INode myNode) {
+	        myLinkedList.append(myNode);
+	    }
+
+	    /*
+	     * Used to pop elements
+	     * */
+	    public INode dequeue() {
+	        return myLinkedList.pop();
+	    }
 }
--- src/com/bridgelabz/linkedlist1/TestStackTest.java
+++ src/com/bridgelabz/linkedlist1/TestStackTest.java
@@ -6,30 +6,63 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TestStackTest {
-	 @Test
-	    public void given3NumbersWhenAddedToStackShouldHaveLastAddedNotes(){
-	        MyNode<Integer>  myFirstNode = new MyNode(70);
-	        MyNode<Integer> mySecondNode = new MyNode(30);
-	        MyNode<Integer>  myThirdNode = new MyNode(56);
-	        Stack myStack = new Stack();
-	        myStack.push(myFirstNode);
-	        myStack.push(mySecondNode);
-	        myStack.push(myThirdNode);
-	        myStack.printStack();
-	        INode peak = myStack.peak();
-	        Assert.assertEquals(myThirdNode , peak);
-	    }
-	    @Test
-	    public void given3NumbersWhenAddedToStackShouldHaveFirstAddedNotes(){
-	        MyNode<Integer>  myFirstNode = new MyNode(56);
-	        MyNode<Integer> mySecondNode = new MyNode(30);
-	        MyNode<Integer>  myThirdNode = new MyNode(70);
-	        Stack myStack = new Stack();
-	        myStack.enqueu(myFirstNode);
-	        myStack.enqueu(mySecondNode);
-	        myStack.enqueu(myThirdNode);
-	        myStack.printStack();
-	        INode peak = myStack.peak();
-	        Assert.assertEquals(myFirstNode , peak);
-	    }
+	@Test
+	public void given3NumbersWhenAddedToStackShouldHaveLastAddedNotes() {
+		MyNode<Integer> myFirstNode = new MyNode(70);
+		MyNode<Integer> mySecondNode = new MyNode(30);
+		MyNode<Integer> myThirdNode = new MyNode(56);
+		Stack myStack = new Stack();
+		myStack.push(myFirstNode);
+		myStack.push(mySecondNode);
+		myStack.push(myThirdNode);
+		myStack.printStack();
+		INode peak = myStack.peak();
+		Assert.assertEquals(myThirdNode, peak);
 	}
+
+	@Test
+	public void given3NumbersInStackWhenPopedTillEmptyShouldReturnTrue() {
+		MyNode<Integer> myFirstNode = new MyNode(70);
+		MyNode<Integer> mySecondNode = new MyNode(30);
+		MyNode<Integer> myThirdNode = new MyNode(56);
+		Stack myStack = new Stack();
+		myStack.push(myFirstNode);
+		myStack.push(mySecondNode);
+		myStack.push(myThirdNode);
+		myStack.printStack();
+		while (!myStack.isEmpty() == true) {
+			myStack.peak();
+			myStack.pop();
+		}
+		Assert.assertTrue(myStack.isEmpty());
+	}
+
+	@Test
+	public void given3NumbersWhenAddedToStackShouldHaveFirstAddedNotes() {
+		MyNode<Integer> myFirstNode = new MyNode(56);
+		MyNode<Integer> mySecondNode = new MyNode(30);
+		MyNode<Integer> myThirdNode = new MyNode(70);
+		Stack myStack = new Stack();
+		myStack.enqueu(myFirstNode);
+		myStack.enqueu(mySecondNode);
+		myStack.enqueu(myThirdNode);
+		myStack.printStack();
+		INode peak = myStack.peak();
+		Assert.assertEquals(myFirstNode, peak);
+	}
+
+	@Test
+	public void given3NumbersWhenAddedToStackAndDequeuedShouldReturnTheSecondElement() {
+		MyNode<Integer> myFirstNode = new MyNode(56);
+		MyNode<Integer> mySecondNode = new MyNode(30);
+		MyNode<Integer> myThirdNode = new MyNode(70);
+		Stack myStack = new Stack();
+		myStack.enqueu(myFirstNode);
+		myStack.enqueu(mySecondNode);
+		myStack.enqueu(myThirdNode);
+		myStack.printStack();
+		INode pop = myStack.dequeue();
+		myStack.printStack();
+		Assert.assertEquals(mySecondNode, pop);
+	}
+}
